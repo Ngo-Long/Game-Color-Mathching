fc6d12927a45176f3195fc402fb1b11d7ed5545d
Ngo Long <ngolong.hh@gamil.com>
2023-08-27 00:44:15 +0700

    1st upgrade

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-import { GAME_STATUS } from './constants'
+import { GAME_STATUS } from './constants.js';
+import { getRandomColorPairs } from './utils.js';
+import {
+  getUlColorElementList,
+  getColorElementList,
+  getTimerElement,
+  getPlayAgainButton,
+  getColorBackground,
+} from './selectors.js';
 
 // Global variables
-let selections = []
-let gameState = GAME_STATUS.PLAYING
+let selections = [];
+let gameState = GAME_STATUS.PLAYING;
 
 // TODOs
 // 1. Generating colors using https://github.com/davidmerfield/randomColor
@@ -10,4 +18,38 @@ let gameState = GAME_STATUS.PLAYING
 // 3. Check win logic
 // 4. Add timer
 // 5. Handle replay click
-console.log('hi')
+
+(() => {
+  // init
+  initColorElementList();
+})();
+
+function initColorElementList() {
+  // add each class index
+  const colorElementList = getColorElementList();
+  if (colorElementList) {
+    colorElementList.forEach((liElement, index) => {
+      liElement.dataset.idx = index;
+    });
+  }
+
+  // attach each target
+  const ulElement = getUlColorElementList();
+  if (ulElement) {
+    ulElement.addEventListener('click', (e) => {
+      const liElement = e.target;
+      if (liElement.tagName !== 'LI') return;
+
+      const index = Number.parseInt(liElement.dataset.idx);
+      handleColorElementList(liElement, index);
+    });
+  }
+}
+
+function handleColorElementList(liElement, index) {
+  liElement.classList.add('active');
+
+  console.log('click', liElement, index);
+}
+
+console.log(getRandomColorPairs(8));
--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,38 @@
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
+  const colorList = [];
+  const hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome'];
+
+  for (let i = 0; i < count; i++) {
+    // randomColor function is provided by https://github.com/davidmerfield/randomColor
+    const color = window.randomColor({
+      luminosity: 'dark',
+      format: hueList[i % hueList.length],
+    });
+
+    colorList.push(color);
+  }
+
+  // double current color list
+  const fullColorList = [...colorList, ...colorList];
+
+  // shuffle it
+  colorPositionShuffle(fullColorList);
+
+  return fullColorList;
+};
+
+function colorPositionShuffle(arr) {
+  if (!Array.isArray(arr) || arr.length <= 2) return arr;
+
+  for (let i = arr.length - 1; i > 1; i--) {
+    let j = Math.floor(Math.random() * i);
+
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  return arr;
 }
